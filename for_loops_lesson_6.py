5fae12071d6fb7f441f5cdb869ed5dbb89b52e8d
Khalid <140214711+k4khalid@users.noreply.github.com>
2024-08-22 22:02:07 +0100

    Delete for_loops_lesson_6.py

--- for_loops_lesson_6.py
+++
@@ -1,9 +0,0 @@
-# Exercise 1 - After flipping a coin 10 times you got this result:
-# Using for loop figure out how many times you got heads
-
-result = ["heads","tails","tails","heads","tails","heads","heads","tails","tails","tails"]
-for i in result:
-    if i != "heads":
-        i = i+1
-
-print(i)
